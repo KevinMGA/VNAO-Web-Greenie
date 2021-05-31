2f813ea65d0d3ed09df33ef4d0db16f87727c8b4
KevinMGA <obsessedtech@outlook.com>
2021-05-31 19:06:56 -0400

    Update

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "ueuynqtqbs");
-define("DB_PASS", "B2HKuXyTzf");
-define("DB_NAME", "ueuynqtqbs");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "greenieboard");
 ?>
