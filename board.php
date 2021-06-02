374b23602acec134b213d990cd94caa255588462
KevinMGA <obsessedtech@outlook.com>
2021-06-02 12:23:32 -0400

    Bug 0.1

--- board.php
+++ board.php
@@ -1,17 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('config/db.php');
 require_once('lib/pdo_db.php');
 require_once('modules/greenieBoard.php');
 
 $Greenie = new GreenieBoard();
+$squad = $Greenie->getSquad($_GET['board']);
+$traps = $Greenie->getTraps();
 
-if(!isset($_GET['board'])) {
-	$set = false;
-} else {
-	$squad = $Greenie->getSquad($_GET['board']);
-	$traps = $Greenie->getTraps();
-	$set = true;
-}
 ?>
 
 <!doctype html>
